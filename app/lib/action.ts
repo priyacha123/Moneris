730b0fbfed479320df02c58d7a88a55fb028bf4f
Priya Kumari <chaudhary21priya@gmail.com>
2025-08-22 21:59:37 +0530

    feat(dashboard/invoices): introduce error boundary (error.tsx and not-found.tsx)

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { log } from "console";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
@@ -29,10 +30,15 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date} )
-  `;
+  try {
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date} )
+    `;
+    } catch (error) {
+      console.error(error);
+    }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices')
@@ -50,11 +56,17 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+  try{
+
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
-  `;
+    `;
+    } catch (error) {
+      console.error(error);
+      
+    }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
